8b57b136a185135d7ad9fae928449f495becde48
Leahnicoles <lnsimonn@hotmail.com>
2023-01-23 21:26:49 +0000

    Fixed layout and completed both exercises

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -8,51 +8,81 @@ public class Runner {
 
 //        SCOTTISH ISLANDS
         List<String> scottishIslands = new ArrayList<>();
-        scottishIslands.add("Tiree");
         scottishIslands.add("Jura");
-        scottishIslands.add("Islay");
         scottishIslands.add("Mull");
         scottishIslands.add("Skye");
         scottishIslands.add("Arran");
         scottishIslands.add("Tresco");
-        scottishIslands.add("Coll");
-        // System.out.println("Skye index: " + scottishIslands.indexOf("Skye"));
-        // scottishIslands.remove("Tresco");
-        // scottishIslands.remove(5);
-        // System.out.println(scottishIslands.size());
-        // System.out.println(scottishIslands.sorted();
-        // Collections.sort(scottishIslands);
-        // System.out.println(scottishIslands);
-        for (String island : scottishIslands) {
-            System.out.println(island);
-        }
 
 //        1. Add "Coll" to the end of the list
+            scottishIslands.add("Coll");
+
 //        2. Add "Tiree" to the start of the list
+            scottishIslands.add(0,"Tiree");
+
 //        3. Add "Islay" after "Jura" and before "Mull"
+            scottishIslands.add(2,"Islay");
+
 //        4. Print out the index position of "Skye"
+            System.out.println("Skye index: " + scottishIslands.indexOf("Skye"));
+
 //        5. Remove "Tresco" from the list by name
+            scottishIslands.remove("Tresco");
+
 //        6. Remove "Arran" from the list by index
+            scottishIslands.remove(5);
+
 //        7. Print the number of islands in your arraylist
+            System.out.println(scottishIslands.size());
+
 //        8. Sort the list alphabetically
+            Collections.sort(scottishIslands);
+            System.out.println(scottishIslands);
+
 //        9. Print out all the islands using a for loop
+            for (String island : scottishIslands) {
+                System.out.println(island);
+            }
 
 
-//        NUMBERS
+//      NUMBERS
         List<Integer> numbers = new ArrayList<>();
         Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
 
         System.out.println("numbers: " + numbers);
 
-//        1. Print out a list of the even integers
-//        2. Print the difference between the largest and smallest value
-//        3. Print True if the list contains a 1 next to a 1 somewhere.
-//        4. Print the sum of the numbers,
-//        5. Print the sum of the numbers...
+//      1. Print out a list of the even integers
+        for (Integer number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+
+
+//      2. Print the difference between the largest and smallest value
+        int max = Collections.max(numbers);
+        int min = Collections.min(numbers);
+        int difference = max - min;
+        System.out.println("The difference between the minimum and maximum number is " + difference );
+
+
+//      3. Print True if the list contains a 1 next to a 1 somewhere.
+
+
+//       4. Print the sum of the numbers.
+        int sum = 0;
+            for (Integer number : numbers) {
+                sum += number;
+                System.out.println("The sum is " + sum);
+            }
+
+
+//       5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count.
 //
 //          So [2, 7, 13, 2] would have sum of 9.
+
 
     }
 
